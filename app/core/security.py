167eab0a90a7edd78a83c357044e8ba6eaff6b75
Tomas Garrido <garridot210@gmail.com>
2025-04-09 13:00:08 -0300

    core: fix tokens required functions

--- app/core/security.py
+++ app/core/security.py
@@ -47,64 +47,78 @@ class JWTManager:
             current_app.logger.warning("Token expirado")
         except jwt.InvalidTokenError as e:
             current_app.logger.error(f"Token inválido: {str(e)}")
-        return None
+        return None   
 
 jwt_manager = JWTManager()
 
+
 def token_required(f):
+        """
+        Decorator to protect routes by requiring a valid JWT token in the request header.
+
+        This decorator checks for the presence and validity of a JWT token in the request headers.    
+
+        Parameters:
+        ----------
+        f : function
+            The function to be wrapped by this decorator.
+        Returns:
+        -------
+        function : 
+            The wrapped function if the token is valid; otherwise, returns a JSON error response.
+        
+        Usage:
+        ------
+        @token_required
+        def protected_route(current_user):
+            # Code for the protected route
+        """
+        @wraps(f)
+        def decorated(*args, **kwargs):            
+
+            try:
+                token = request.headers.get('x-access-token')
+                if not token: return jsonify({'message': 'x-access-token is missing.'}), 403
+                # Decode the JWT token.
+                data = jwt.decode(token, Config.SECRET_KEY, algorithms=[Config.JWT_ALGORITHM])                
+                # Search for the user based on the user_id in the token.
+                current_user = User.query.filter_by(id=data['sub']).first()
+                # Check if the user exists.
+                if current_user is None:  return jsonify({'message': 'User not found.'}), 404
+
+            except jwt.ExpiredSignatureError:
+                return jsonify({'message': 'Token expired. Please refresh your token.'}), 401
+            except jwt.InvalidTokenError:
+                return jsonify({'message': 'Invalid token.'}), 403
+            except Exception as e:
+                return jsonify({'message': 'Token is invalid or corrupted.', 'error': str(e)}), 403
+
+            # Continue executing the function if the token is valid.
+            return f(current_user, *args, **kwargs)
+
+        return decorated    
+
+
+def refresh_token_required(f):
     """
-    Decorator to protect routes by requiring a valid JWT token in the request header.
-
-    This decorator checks for the presence and validity of a JWT token in the request headers.
-    If the token is valid, it allows the request to proceed with the authenticated user. 
-    If invalid or missing, it returns an appropriate JSON response with an error message.
-
-    Parameters:
-    ----------
-    f : function
-        The function to be wrapped by this decorator.
-
-    Returns:
-    -------
-    function : 
-        The wrapped function if the token is valid; otherwise, returns a JSON error response.
-
-    JSON Responses:
-    ---------------
-    403 : {'message': 'Token is missing.'}
-        If no token is provided in the request headers.
-    404 : {'message': 'User not found.'}
-        If the token is valid but no user is found with the ID in the token.
-    401 : {'message': 'Token expired. Please refresh your token.'}
-        If the token has expired.
-    403 : {'message': 'Invalid token.'}
-        If the token is malformed or invalid.
-    403 : {'message': 'Token is invalid or corrupted.'}
-        For any other unexpected errors with the token.
-
-    Usage:
-    ------
-    @token_required
-    def protected_route(current_user):
-        # Code for the protected route
+    Decorator specific to token refresh endpoints.
     """
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.headers.get('x-access-token')
-        
-        if not token:
-            return jsonify({'message': 'Token is missing.'}), 403
 
         try:
+            token = request.headers.get('x-refresh-token')
+            if not token: return jsonify({'message': 'x-refresh-token is missing.'}), 403
             # Decode the JWT token.
-            data = jwt.decode(token, Config.SECRET_KEY, algorithms=['HS256'])       
+            data = jwt.decode(token, Config.SECRET_KEY, algorithms=[Config.JWT_ALGORITHM])                
+            
+            if not data or data['scope'] != 'refresh_token': 
+                return jsonify({"error": "Invalid refresh token"}), 401
             
             # Search for the user based on the user_id in the token.
             current_user = User.query.filter_by(id=data['sub']).first()
-
             # Check if the user exists.
-            if current_user is None:
-                return jsonify({'message': 'User not found.'}), 404
+            if current_user is None:  return jsonify({'message': 'User not found.'}), 404
 
         except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Token expired. Please refresh your token.'}), 401
@@ -116,26 +130,4 @@ def token_required(f):
         # Continue executing the function if the token is valid.
         return f(current_user, *args, **kwargs)
 
-    return decorated
-
-
-def refresh_token_required(f):
-    """
-    Decorator specific to token refresh endpoints.
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        auth_header = request.headers.get('Authorization')
-        if not auth_header or not auth_header.startswith('Bearer '):
-            return jsonify({"error": "Token missing or invalid"}), 401
-            
-        token = auth_header.split(' ')[1]
-        payload = verify_token(token)
-        
-        if not payload or payload.get('scope') != 'refresh_token':
-            return jsonify({"error": "Invalid refresh token"}), 401
-            
-        request.user_id = payload['sub']
-        return f(*args, **kwargs)
-        
-    return decorated_function
+    return decorated 
